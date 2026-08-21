99ece134085a4b8c48bec33b272a8cc039e7881e
user <60944239+wipedlifepotato@users.noreply.github.com>
2026-08-21 06:34:25 +0000

    Initial Shit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+#https://transmission-rpc.readthedocs.io/en/v7.0.12/
+#https://github.com/transmission/transmission/blob/main/docs/rpc-spec.md
+from transmission_rpc import Client
+
+def main():
+	
+	pass
+
+if __name__ == "__main__":
+	main()
